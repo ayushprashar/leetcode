f5701436cbd78a9250994f65ccdd27c44d48d6eb
Ayush Prashar <ayush.prashar@gmail.com>
2020-09-13 19:27:23 +0530

    Day 2 problem 4

---
+++ CountAndSay.java
@@ -0,0 +1,22 @@
+class CountAndSay {
+	public String countAndSay(int n) {
+        if(n <= 0)
+            return "";
+        String result = "1";
+        while(n>1) {
+            StringBuilder sb = new StringBuilder();
+            for(int i = 0; i < result.length(); i++) {
+                int count = 1;
+                while(i+1 < result.length() &&
+                     result.charAt(i) == result.charAt(i+1)) {
+                    count++;
+                    i++;
+                }
+                sb.append(count).append(result.charAt(i));
+            }
+            result = sb.toString();
+            n--;
+        }
+        return result;
+    }
+}
