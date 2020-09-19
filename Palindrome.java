65a1a12f840eeb0fad0d5b7e1942ee707422d806
Ayush Prashar <ayush.prashar@gmail.com>
2020-09-19 11:16:41 +0530

    Day 8 Problem 4 Improved

--- Palindrome.java
+++ Palindrome.java
@@ -1,5 +1,5 @@
 class Palindrome {
-    public boolean isPalindrome(String s) {
+    /*public boolean isPalindrome(String s) {
         StringBuilder sb = new StringBuilder();
         for(int i = 0; i < s.length(); i++) {
             if(Character.isLetterOrDigit(s.charAt(i))) {
@@ -11,6 +11,26 @@ class Palindrome {
         for(int i = 0, j = sLen - 1; i< sLen && j >= 0; i++, j--) {
             if(s.charAt(i) != s.charAt(j))
                 return false;
+        }
+        return true;
+    }*/
+    public boolean isPalindrome(String s) {
+        int sLen = s.length();
+        for(int i = 0, j = sLen - 1; i< sLen && j >= 0 && j > i;) {
+            if(!Character.isLetterOrDigit(s.charAt(i))) {
+                i++;
+                continue;
+            }
+            if(!Character.isLetterOrDigit(s.charAt(j))) {
+                j--;
+                continue;
+            }
+            if(Character.toLowerCase(s.charAt(i)) != Character.toLowerCase(s.charAt(j)))
+                return false;
+            else {
+                i++;
+                j--;
+            }
         }
         return true;
     }
