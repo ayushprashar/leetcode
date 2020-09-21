821144650b7fa675c57a4105f45cdf570df53bce
Ayush Prashar <ayush.prashar@gmail.com>
2020-09-21 08:45:21 +0530

    Day 10 Problem 1

---
+++ TrailingZeroFact.java
@@ -0,0 +1,10 @@
+class TrailingZeroFact {
+    public int trailingZeroes(int n) {
+        int count = 0;
+        for(int i = 5; 1 <= n/i; i *= 5) {
+            count += n/i;
+            
+        }
+        return count;
+    }
+}
